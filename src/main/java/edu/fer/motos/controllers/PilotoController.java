6b828e256f7f5f6caaa185b95067927248ee71ab
Fer <feroxtv@gmail.com>
2024-11-21 11:40:52 +0100

    Controllers added

---
+++ src/main/java/edu/fer/motos/controllers/PilotoController.java
@@ -0,0 +1,10 @@
+package edu.fer.motos.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pilotos")
+public class PilotoController {
+
+}
