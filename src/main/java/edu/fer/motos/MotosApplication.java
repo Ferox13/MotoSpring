1ac7f84eba12393501be05813085e21705147da7
Fer <feroxtv@gmail.com>
2024-11-21 10:41:09 +0100

    Alert for correct initiation

--- src/main/java/edu/fer/motos/MotosApplication.java
+++ src/main/java/edu/fer/motos/MotosApplication.java
@@ -1,7 +1,9 @@
 package edu.fer.motos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MotosApplication {
@@ -10,4 +12,11 @@ public class MotosApplication {
 		SpringApplication.run(MotosApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			// Código a ejecutar al inicio de la aplicación
+			System.out.println("La aplicación ha iniciado correctamente.");
+		};
+	}
 }
