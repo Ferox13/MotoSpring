947eb32c2329036d47b97b75f7a345b3368c51f5
Fer <feroxtv@gmail.com>
2024-11-21 13:41:08 +0100

    Sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -24,30 +24,30 @@ INSERT INTO carreras (temporada, posicion, piloto_id, circuito_id) VALUES
 ('2022', 3, 3, 1), -- Francesco Bagnaia tercero en Jerez
 ('2022', 1, 3, 2), -- Francesco Bagnaia gana en Cataluña
 ('2022', 2, 2, 2), -- Fabio Quartararo segundo en Cataluña
-('2022', 3, 6, 2), -- Enea Bastianini tercero en Cataluña
+('2022', 6, 6, 2), -- Enea Bastianini sexto en Cataluña
 ('2022', 1, 6, 3), -- Enea Bastianini gana en Mugello
-('2022', 2, 4, 3), -- Joan Mir segundo en Mugello
-('2022', 3, 2, 3), -- Fabio Quartararo tercero en Mugello
+('2022', 4, 4, 3), -- Joan Mir cuarto en Mugello
+('2022', 5, 2, 3), -- Fabio Quartararo quinto en Mugello
 ('2022', 1, 2, 4), -- Fabio Quartararo gana en Assen
 ('2022', 2, 1, 4), -- Marc Márquez segundo en Assen
-('2022', 3, 5, 4), -- Aleix Espargaró tercero en Assen
+('2022', 7, 5, 4), -- Aleix Espargaró séptimo en Assen
 
 -- Temporada 2023
 ('2023', 1, 3, 1), -- Francesco Bagnaia gana en Jerez
 ('2023', 2, 2, 1), -- Fabio Quartararo segundo en Jerez
 ('2023', 3, 1, 1), -- Marc Márquez tercero en Jerez
 ('2023', 1, 2, 2), -- Fabio Quartararo gana en Cataluña
-('2023', 2, 5, 2), -- Aleix Espargaró segundo en Cataluña
-('2023', 3, 3, 2), -- Francesco Bagnaia tercero en Cataluña
+('2023', 5, 5, 2), -- Aleix Espargaró quinto en Cataluña
+('2023', 4, 3, 2), -- Francesco Bagnaia cuarto en Cataluña
 ('2023', 1, 6, 3), -- Enea Bastianini gana en Mugello
-('2023', 2, 3, 3), -- Francesco Bagnaia segundo en Mugello
-('2023', 3, 4, 3), -- Joan Mir tercero en Mugello
+('2023', 6, 3, 3), -- Francesco Bagnaia sexto en Mugello
+('2023', 7, 4, 3), -- Joan Mir séptimo en Mugello
 ('2023', 1, 1, 4), -- Marc Márquez gana en Assen
-('2023', 2, 3, 4), -- Francesco Bagnaia segundo en Assen
-('2023', 3, 5, 4), -- Aleix Espargaró tercero en Assen
+('2023', 4, 3, 4), -- Francesco Bagnaia cuarto en Assen
+('2023', 5, 5, 4), -- Aleix Espargaró quinto en Assen
 ('2023', 1, 3, 5), -- Francesco Bagnaia gana en Phillip Island
-('2023', 2, 2, 5), -- Fabio Quartararo segundo en Phillip Island
-('2023', 3, 6, 5), -- Enea Bastianini tercero en Phillip Island
+('2023', 6, 2, 5), -- Fabio Quartararo sexto en Phillip Island
+('2023', 7, 6, 5), -- Enea Bastianini séptimo en Phillip Island
 ('2023', 1, 5, 6), -- Aleix Espargaró gana en Sepang
-('2023', 2, 4, 6), -- Joan Mir segundo en Sepang
-('2023', 3, 1, 6); -- Marc Márquez tercero en Sepang
+('2023', 4, 4, 6), -- Joan Mir cuarto en Sepang
+('2023', 5, 1, 6); -- Marc Márquez quinto en Sepang
